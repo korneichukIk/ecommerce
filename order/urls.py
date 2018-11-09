84f2e5790ebae799f5b4223dda31f37b54d3f429
korneichukIk <korneychuk.ivan.betta.version@gmail.com>
2018-11-09 19:40:19 +0100

    Add PDF generator

--- order/urls.py
+++ order/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import order_create
+from .views import order_create, order_pdf
 
 urlpatterns = [
+    path('admin/order/<order_id>/pdf/', order_pdf, name = 'order_pdf'),
     path('create/', order_create, name = 'order_create'),
 ]
